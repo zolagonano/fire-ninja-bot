e51342e9fe2566c0b81cab25f392403f946f7dd1
Zola Gonano <zolagonano@protonmail.com>
2024-04-25 14:54:54 +0000

    Use enum for commands, replace fetch_source with fetch_sources

--- src/lib.rs
+++ src/lib.rs
@@ -19,14 +19,17 @@ const HELP_MESSAGE: &str = "Fire Ninja Bot allows you to access proxies to bypas
 - /vmess: Fetches and provides a list of VMess proxies.
 ";
 
-async fn fetch_source(source: &str) -> core::result::Result<String, &'static str> {
-    let response = reqwest::get(source)
-        .await
-        .map_err(|_e| "Failed to fetch the source")?
-        .text()
-        .await
-        .map_err(|_e| "Failed to load the content")?;
-    Ok(response)
+async fn fetch_sources(sources: &[&str]) -> String {
+    let mut content = String::new();
+    for source in sources {
+        if let Ok(response) = reqwest::get(*source).await {
+            if let Ok(text) = response.text().await {
+                content.push_str(&text);
+            }
+        }
+    }
+
+    content
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -45,6 +48,25 @@ struct TelegramChat {
     id: i64,
 }
 
+enum Command {
+    MTProxy,
+    Shadowsocks,
+    VMess,
+    Help,
+}
+
+impl Command {
+    pub fn from_str(command: &str) -> Option<Self> {
+        match command {
+            "/mtproxy" => Some(Self::MTProxy),
+            "/ss" | "/shadowsocks" => Some(Self::Shadowsocks),
+            "/vmess" => Some(Self::VMess),
+            "/start" | "/help" => Some(Self::Help),
+            _ => None,
+        }
+    }
+}
+
 #[event(fetch)]
 async fn main(mut req: Request, _env: Env, _ctx: Context) -> Result<Response> {
     let update: Result<TelegramUpdate> = req.json().await;
@@ -58,66 +80,58 @@ async fn main(mut req: Request, _env: Env, _ctx: Context) -> Result<Response> {
         let chat_id = message.chat.id;
         let text = message.text.to_lowercase();
 
-        let response_text = match text.as_str() {
-            "/mtproxy" => {
+        let response_text = match Command::from_str(text.as_str()) {
+            Some(Command::MTProxy) => {
                 // NOTE: HashSet is used here to deduplicate the proxy links
                 let mut proxy_list = HashSet::new();
-
-                for source in MTPROTO_SOURCES {
-                    if let Ok(raw_proxies) = fetch_source(source).await {
-                        let proxies = proxy_scraper::Scraper::scrape_mtproxy(&raw_proxies);
-                        for proxy in proxies {
-                            proxy_list.insert(format!(
-                                "[Host: {} Port: {}]({})",
-                                proxy.host,
-                                proxy.port,
-                                proxy.to_url()
-                            ));
-                        }
-                    }
+                let raw_proxies = fetch_sources(MTPROTO_SOURCES).await;
+                let proxies = proxy_scraper::Scraper::scrape_mtproxy(&raw_proxies);
+                for proxy in proxies {
+                    proxy_list.insert(format!(
+                        "[Host: {} Port: {}]({})",
+                        proxy.host,
+                        proxy.port,
+                        proxy.to_url()
+                    ));
                 }
+
                 proxy_list
                     .into_iter()
                     .take(15)
                     .collect::<Vec<_>>()
                     .join("\n﹌﹌﹌\n")
             }
-            "/ss" | "/shadowsocks" => {
+            Some(Command::Shadowsocks) => {
                 let mut proxy_list = HashSet::new();
 
-                for source in SHADOWSOCKS_SOURCES {
-                    if let Ok(raw_proxies) = fetch_source(source).await {
-                        let proxies = proxy_scraper::Scraper::scrape_shadowsocks(&raw_proxies);
-                        for proxy in proxies {
-                            proxy_list.insert(format!("`{}`", proxy.to_url()));
-                        }
-                    }
+                let raw_proxies = fetch_sources(SHADOWSOCKS_SOURCES).await;
+                let proxies = proxy_scraper::Scraper::scrape_shadowsocks(&raw_proxies);
+                for proxy in proxies {
+                    proxy_list.insert(format!("`{}`", proxy.to_url()));
                 }
+
                 proxy_list
                     .into_iter()
-                    .take(15)
+                    .take(12)
                     .collect::<Vec<_>>()
                     .join("\n﹌﹌﹌\n")
             }
-            "/vmess" => {
+            Some(Command::VMess) => {
                 let mut proxy_list = HashSet::new();
 
-                for source in VMESS_SOURCES {
-                    if let Ok(raw_proxies) = fetch_source(source).await {
-                        let proxies = proxy_scraper::Scraper::scrape_vmess(&raw_proxies);
-                        for proxy in proxies {
-                            proxy_list.insert(format!("`{}`", proxy.to_url()));
-                        }
-                    }
+                let raw_proxies = fetch_sources(VMESS_SOURCES).await;
+                let proxies = proxy_scraper::Scraper::scrape_vmess(&raw_proxies);
+                for proxy in proxies {
+                    proxy_list.insert(format!("`{}`", proxy.to_url()));
                 }
                 proxy_list
                     .into_iter()
-                    .take(10)
+                    .take(8)
                     .collect::<Vec<_>>()
                     .join("\n﹌﹌﹌\n")
             }
-            "/help" | "/start" => HELP_MESSAGE.to_string(),
-            _ => "Invalid command, use /help to get list of available commands.".to_string(),
+            Some(Command::Help) => HELP_MESSAGE.to_string(),
+            None => "Invalid command, use /help to get list of available commands.".to_string(),
         };
 
         let response = json!({
